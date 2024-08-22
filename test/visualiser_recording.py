89243d4afe28523d89b8a0734259321d0d3a5893
Joooomi <128105594+Joooomi@users.noreply.github.com>
2024-08-22 14:50:44 +0900

    Add files via upload
    visualiserに動画保存機能を追加

---
+++ test/visualiser_recording.py
@@ -0,0 +1,215 @@
+import leap
+import numpy as np
+import cv2
+import time #added code
+
+_TRACKING_MODES = {
+    leap.TrackingMode.Desktop: "Desktop",
+    leap.TrackingMode.HMD: "HMD",
+    leap.TrackingMode.ScreenTop: "ScreenTop",
+}
+
+#added code
+fourcc = cv2.VideoWriter_fourcc(*"mp4v") 
+out = cv2.VideoWriter('output.mp4', fourcc, 250.0, (700, 500))
+#end
+
+class Canvas:
+    def __init__(self):
+        self.name = "Python Gemini Visualiser"
+        self.screen_size = [500, 700]
+        self.hands_colour = (255, 255, 255)
+        self.font_colour = (0, 255, 44)
+        self.hands_format = "Skeleton"
+        self.output_image = np.zeros((self.screen_size[0], self.screen_size[1], 3), np.uint8)
+        self.tracking_mode = None
+
+    def set_tracking_mode(self, tracking_mode):
+        self.tracking_mode = tracking_mode
+
+    def toggle_hands_format(self):
+        self.hands_format = "Dots" if self.hands_format == "Skeleton" else "Skeleton"
+        print(f"Set hands format to {self.hands_format}")
+
+    def get_joint_position(self, bone):
+        if bone:
+            return int(bone.x + (self.screen_size[1] / 2)), int(bone.z + (self.screen_size[0] / 2))
+        else:
+            return None
+
+    def render_hands(self, event):
+        # Clear the previous image
+        self.output_image[:, :] = 0
+
+        cv2.putText(
+            self.output_image,
+            f"Tracking Mode: {_TRACKING_MODES[self.tracking_mode]}",
+            (10, self.screen_size[0] - 10),
+            cv2.FONT_HERSHEY_SIMPLEX,
+            0.5,
+            self.font_colour,
+            1,
+        )
+
+        if len(event.hands) == 0:
+            return
+
+        for i in range(0, len(event.hands)):
+            hand = event.hands[i]
+            for index_digit in range(0, 5):
+                digit = hand.digits[index_digit]
+                for index_bone in range(0, 4):
+                    bone = digit.bones[index_bone]
+                    if self.hands_format == "Dots":
+                        prev_joint = self.get_joint_position(bone.prev_joint)
+                        next_joint = self.get_joint_position(bone.next_joint)
+                        if prev_joint:
+                            cv2.circle(self.output_image, prev_joint, 2, self.hands_colour, -1)
+
+                        if next_joint:
+                            cv2.circle(self.output_image, next_joint, 2, self.hands_colour, -1)
+
+                    if self.hands_format == "Skeleton":
+                        wrist = self.get_joint_position(hand.arm.next_joint)
+                        elbow = self.get_joint_position(hand.arm.prev_joint)
+                        if wrist:
+                            cv2.circle(self.output_image, wrist, 3, self.hands_colour, -1)
+
+                        if elbow:
+                            cv2.circle(self.output_image, elbow, 3, self.hands_colour, -1)
+
+                        if wrist and elbow:
+                            cv2.line(self.output_image, wrist, elbow, self.hands_colour, 2)
+
+                        bone_start = self.get_joint_position(bone.prev_joint)
+                        bone_end = self.get_joint_position(bone.next_joint)
+
+                        if bone_start:
+                            cv2.circle(self.output_image, bone_start, 3, self.hands_colour, -1)
+
+                        if bone_end:
+                            cv2.circle(self.output_image, bone_end, 3, self.hands_colour, -1)
+
+                        if bone_start and bone_end:
+                            cv2.line(self.output_image, bone_start, bone_end, self.hands_colour, 2)
+
+                        if ((index_digit == 0) and (index_bone == 0)) or (
+                            (index_digit > 0) and (index_digit < 4) and (index_bone < 2)
+                        ):
+                            index_digit_next = index_digit + 1
+                            digit_next = hand.digits[index_digit_next]
+                            bone_next = digit_next.bones[index_bone]
+                            bone_next_start = self.get_joint_position(bone_next.prev_joint)
+                            if bone_start and bone_next_start:
+                                cv2.line(
+                                    self.output_image,
+                                    bone_start,
+                                    bone_next_start,
+                                    self.hands_colour,
+                                    2,
+                                )
+
+                        if index_bone == 0 and bone_start and wrist:
+                            cv2.line(self.output_image, bone_start, wrist, self.hands_colour, 2)
+
+
+class TrackingListener(leap.Listener):
+    def __init__(self, canvas):
+        self.canvas = canvas
+
+    def on_connection_event(self, event):
+        pass
+
+    def on_tracking_mode_event(self, event):
+        self.canvas.set_tracking_mode(event.current_tracking_mode)
+        print(f"Tracking mode changed to {_TRACKING_MODES[event.current_tracking_mode]}")
+
+    def on_device_event(self, event):
+        try:
+            with event.device.open():
+                info = event.device.get_info()
+        except leap.LeapCannotOpenDeviceError:
+            info = event.device.get_info()
+
+        print(f"Found device {info.serial}")
+
+    def on_tracking_event(self, event):
+        self.canvas.render_hands(event)
+
+
+def main():
+    canvas = Canvas()
+
+    print(canvas.name)
+    print("")
+    print("Press <key> in visualiser window to:")
+    print("  x: Exit")
+    print("  h: Select HMD tracking mode")
+    print("  s: Select ScreenTop tracking mode")
+    print("  d: Select Desktop tracking mode")
+    print("  f: Toggle hands format between Skeleton/Dots")
+
+    tracking_listener = TrackingListener(canvas)
+
+    connection = leap.Connection()
+    connection.add_listener(tracking_listener)
+
+    running = True
+
+    #added
+    prev_time = time.time()
+    fcount = 0
+    record = False
+    #end
+
+    with connection.open():
+        connection.set_tracking_mode(leap.TrackingMode.Desktop)
+        canvas.set_tracking_mode(leap.TrackingMode.Desktop)
+
+        while running:            
+            #added code
+            curr_time = time.time()
+            diff_time = curr_time - prev_time
+            fcount +=1
+            if int(diff_time % 60) == 1:
+                prev_time = curr_time
+                print(f"fps = {fcount} frames/sec") # print canvas' frame rate
+                fcount = 0
+
+            #end
+            cv2.imshow(canvas.name, canvas.output_image)
+            
+            #added code
+            frame = canvas.output_image
+            if frame.shape[1] != 700 or frame.shape[0] != 500:
+                frame = cv2.resize(frame, (700, 500))
+
+            if frame.dtype != 'uint8':
+                frame = frame.astype('uint8')               
+
+            if record is True:
+                out.write(frame)
+            #end
+            
+            key = cv2.waitKey(1)
+
+            #added code
+            if key == ord("r"):
+                record = not record             
+            #end
+
+            if key == ord("x"):
+                break
+            elif key == ord("h"):
+                connection.set_tracking_mode(leap.TrackingMode.HMD)
+            elif key == ord("s"):
+                connection.set_tracking_mode(leap.TrackingMode.ScreenTop)
+            elif key == ord("d"):
+                connection.set_tracking_mode(leap.TrackingMode.Desktop)
+            elif key == ord("f"):
+                canvas.toggle_hands_format()
+        out.release() #added code
+
+
+if __name__ == "__main__":
+    main()
